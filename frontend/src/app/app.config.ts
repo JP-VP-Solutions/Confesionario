36c98378cada294e76a4f53a1f6d4f86e66284f1
valentinopretto <valentinopretto055@gmail.com>
2025-10-22 16:08:05 -0300

    fixed styles and deployed succesfully

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -9,7 +9,7 @@ import { authInterceptor } from './components/auth/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
